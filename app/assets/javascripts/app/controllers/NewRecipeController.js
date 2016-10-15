b4794c5fa4696b69ffafcb2628eb347e38ae405b
Daniel Brad <danieltbrad@gmail.com>
2016-10-15 15:44:23 -0700

    started new recipe form; created dynamically generated direction step fields

--- app/assets/javascripts/app/controllers/NewRecipeController.js
+++ app/assets/javascripts/app/controllers/NewRecipeController.js
@@ -2,6 +2,16 @@ NewRecipeController.$inject = ["$state", "$stateParams"];
 function NewRecipeController($state, $stateParams) {
 
   ctrl = this
+  ctrl.recipe = { title: "", directions: [{id: 'step1', desc: ""}] }
+
+  ctrl.addNewStep = function() {
+    var newStepNo = ctrl.recipe.directions.length+1;
+    ctrl.recipe.directions.push({'id':'step'+newStepNo});
+  };
+  ctrl.showAddStep = function(step) {
+  return step.id === ctrl.recipe.directions[ctrl.recipe.directions.length-1].id;
+};
+
 
 }
 angular
