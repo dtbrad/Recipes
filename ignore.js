0250e6278486146f00a6fc6c0d07f9d715a624ea
Daniel Brad <danieltbrad@gmail.com>
2016-10-17 14:09:09 -0700

    added dynamically created ingredient fields

--- ignore.js
+++ ignore.js
@@ -5,16 +5,37 @@ function NewRecipeController($state, $stateParams, DataService, ingredients) {
   ctrl.ingredients = ingredients.data
   ctrl.recipe = { title: "", directions: [{id: 'step1', desc: ""}], ingredients: [{id: 'ing1', desc: "dan"}] }
 
+  // ctrl.addNewStep = function() {
+  //   var newStepNo = ctrl.recipe.directions.length+1;
+  //   ctrl.recipe.directions.push({'id':'step'+newStepNo});
+  // };
+
   ctrl.addNewField = function(field, array) {
     var newFieldNo = array.length+1;
     array.push({'id':'step'+newFieldNo});
   };
 
+  // ctrl.showAddStep = function(step) {
+  //   return step.id === ctrl.recipe.directions[ctrl.recipe.directions.length-1].id;
+  // };
+
   ctrl.showAddField = function(field, array) {
     return field.id === array[array.length-1].id;
   };
 
+  // ctrl.addNewIng = function() {
+  //   var newIngNo = ctrl.recipe.ingredients.length+1;
+  //   ctrl.recipe.ingredients.push({'id':'step'+newIngNo});
+  // };
+
+  // ctrl.showAddNewIng = function(ing) {
+  //   return ing.id === ctrl.recipe.ingredients[ctrl.recipe.ingredients.length-1].id;
+  // };
+
+
+
   ctrl.submit = function(){
+    debugger;
     var array = [];
     ctrl.recipe.directions.forEach(function(c){array.push(c.desc)});
     ctrl.recipe.directions = array
