d3d456d15e057f0df6c3393ac041e627a10c8a98
Daniel Brad <danieltbrad@gmail.com>
2016-10-19 13:12:59 -0700

    refactored and added to new recipe form validations

--- app/assets/javascripts/app/controllers/NewRecipeController.js
+++ app/assets/javascripts/app/controllers/NewRecipeController.js
@@ -29,12 +29,6 @@ function NewRecipeController($state, $stateParams, DataService, ingredients) {
     return field.id === array[array.length-1].id;
   };
 
-  ctrl.noEmpties = function(){
-
-    if ((ctrl.recipe.ingredients_attributes.filter(function(i){ return (i.name == "" || i.quantityPrep == "")})).length > 0)
-    { return true }
-  }
-
   ctrl.setIngredient = function(selectedValue, ingredientEntry){
     if (selectedValue)
     { ingredientEntry.name = selectedValue.name }
@@ -42,12 +36,12 @@ function NewRecipeController($state, $stateParams, DataService, ingredients) {
     { ingredientEntry.name = "" }
   }
 
-
   ctrl.submit = function(){
     ctrl.recipe.directions = ctrl.recipe.directions.map(function(d){return d.content});
     DataService.postRecipe(ctrl.recipe)
     .then(function(result){
       $state.go('home.recipe', {id: result.data.id});
+      alert("recipe created")
     });
   };
 
